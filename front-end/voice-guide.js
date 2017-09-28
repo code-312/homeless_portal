3b42311eb20f421e51524a00a96c75a8611f9348
roblouie <rob.j.louie@gmail.com>
2017-09-28 16:39:37 -0500

    Voice recognition + Text to speech proof of concept

---
+++ front-end/voice-guide.js
@@ -0,0 +1,82 @@
+window.onload = startVoiceGuide;
+
+function startVoiceGuide() {
+  window.speechSynthesis.cancel();
+  const recognition = new webkitSpeechRecognition();
+
+  const categories = [
+    {
+      name: 'FOOD',
+      keywords: ['hungry', 'food', 'eat'],
+      url: './food.html',
+    },
+    {
+      name: 'SHELTER',
+      keywords: ['shelter', 'sleep', 'warm'],
+      url: './shelter.html',
+    },
+    {
+      name: 'JOB',
+      keywords: ['job', 'employ', 'work'],
+      url: './job.html',
+    },
+  ];
+
+  delay(200)
+    .then(() => speakToUser("What would you like help with?"))
+    .then(() => delay(300))
+    .then(() => speakToUser("You can say things like I'm hungry, I want a job, or a place to sleep."))
+    .then(listenForResponse)
+    .then(parseUserResponse)
+    .catch(error => console.error);
+
+  function speakToUser(textToSay) {
+    const message = new SpeechSynthesisUtterance(textToSay);
+    console.log(message);
+    const promise = new Promise(resolve => {
+      message.onend = (result) => {
+        console.log(result);
+        resolve();
+      }
+    });
+    window.speechSynthesis.speak(message);
+    return promise;
+  }
+
+  function listenForResponse() {
+    document.getElementById("listening-icon").classList.remove('hidden');
+    const promise = new Promise(resolve => {
+      recognition.onresult = resolve;
+    });
+    recognition.start();
+    return promise;
+  }
+
+  function parseUserResponse(event) {
+    const spoken = event.results[0][0].transcript;
+    const confidence = event.results[0][0].confidence;
+    console.log(`Speech recognition confidence in it's understanding of the speech is: ${confidence}`);
+    console.log(`Speech was understood as: ${spoken}`);
+
+    const chosenCategory = categories.find(category => isKeywordInSpeech(spoken, category));
+
+    document.getElementById("listening-icon").classList.add('hidden');
+
+    if (chosenCategory === undefined) {
+      speakToUser("Sorry, I can't find what you're looking for");
+    } else {
+      window.location.href = chosenCategory.url;
+    }
+  }
+
+  function isKeywordInSpeech(spoken, category) {
+    const foundKeyword = category.keywords.find(keyword => spoken.includes(keyword));
+    return foundKeyword !== undefined;
+  }
+
+  function delay(t) {
+    return new Promise(function(resolve) {
+      setTimeout(resolve, t)
+    });
+  }
+}
